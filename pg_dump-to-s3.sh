350f211d173a5d03d510b6b4ab73fb19bbefa836
Padungsak Kanyapila <kanyapila101@gmail.com>
2024-09-18 15:01:57 +0700

    update.sql

--- pg_dump-to-s3.sh
+++ pg_dump-to-s3.sh
@@ -43,13 +43,13 @@ for db in "${DBS[@]}"; do
     echo "   -> backing up $db..."
 
     # Dump database
-    pg_dump -Fc -h $PG_HOST -U $PG_USER -W $PG_PASS -p $PG_PORT $db > /tmp/"$FILENAME".dump
+    pg_dump -Fc -h $PG_HOST -U $PG_USER -W $PG_PASS -p $PG_PORT $db > /tmp/"$FILENAME".sql
 
     # Copy to S3
-    aws s3 cp /tmp/"$FILENAME".dump s3://$S3_PATH/"$FILENAME".dump --storage-class STANDARD_IA
+    aws s3 cp /tmp/"$FILENAME".sql s3://$S3_PATH/"$FILENAME".sql --storage-class STANDARD_IA
 
     # Delete local file
-    rm /tmp/"$FILENAME".dump
+    rm /tmp/"$FILENAME".sql
 
     # Log
     echo "      ...database $db has been backed up"
